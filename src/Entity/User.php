7a8eabfcdd686de47be5ce7c3caf28f702ef2122
Yanis <144674873+Yanistone@users.noreply.github.com>
2025-11-04 11:14:38 +0100

    feat(security): implement user authentication and registration system
    add login and registration functionality with custom authenticator
    create security controller with login/logout routes
    add registration controller with form handling
    implement user entity with validation constraints
    add login and registration twig templates
    configure security.yaml with custom authenticator

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -33,8 +35,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column]
-    private ?int $XP = null;
+    #[ORM\Column(options: ['default' => 0])]
+    private ?int $XP = 0;
 
     /**
      * @var Collection<int, Language>
