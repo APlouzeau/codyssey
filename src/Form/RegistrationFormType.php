37cb5ebdb8d62ad0a6cdb6e582c0f673c01eadf4
Trouche Xavier <xavier.trouche@ecirtp.fr>
2025-11-04 15:14:18 +0000

    mise en place du formulaire login et register avec notion de controle(12caractère, Majsucule, minuscule, chiffre...)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,15 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex; // Ajoutez cette ligne
 
 class RegistrationFormType extends AbstractType
 {
@@ -22,19 +21,27 @@ class RegistrationFormType extends AbstractType
             ->add('pseudo', TextType::class)
             ->add('email', EmailType::class)
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'min' => 12, // Changement ici : minimum 12 caractères
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
                         'max' => 4096,
+                    ]),
+                    new Regex([ // Contrainte de complexité
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])/',
+                        // Explication de la Regex :
+                        // ^                  # Début de la chaîne
+                        // (?=.*[a-z])        # Doit contenir au moins une minuscule
+                        // (?=.*[A-Z])        # Doit contenir au moins une majuscule
+                        // (?=.*\d)           # Doit contenir au moins un chiffre
+                        // (?=.*[^\w\s])      # Doit contenir au moins un caractère spécial (tout ce qui n'est pas lettre, chiffre ou espace)
+                        // Note : on n'a pas besoin de l'ancre de fin ($) car la contrainte Length gère la longueur.
+                        'message' => 'Votre mot de passe doit contenir au moins 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial.',
                     ]),
                 ],
             ])
